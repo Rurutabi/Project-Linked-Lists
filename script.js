2668ff657971498a2226f8b2d36f548f23467fe3
Sira <siraphop_2538@hotmail.com>
2023-11-26 13:37:16 +1100

    Create Node and LinkedList clsses

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('test');
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+const n1 = new Node(100);
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+}
+
+const theLink = new LinkedList();
