2079a76fb9214b6cabd4143322ca85cf808f0e13
Sira <siraphop_2538@hotmail.com>
2023-11-28 00:30:04 +1100

    printSize, getHead, geTail and atIndex

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ class LinkedList {
     this.size = 0;
   }
 
-  append(val) {
+  _append(val) {
     if (this.head === null) {
       this.head = new Node(val);
       return;
@@ -29,7 +29,7 @@ class LinkedList {
     // console.log(curr.next);
   }
 
-  prepend(val) {
+  _prepend(val) {
     //     1. Create a new node with the desired element.
     // 2. Set the next pointer of the new node to the current head of the linked list.
     // 3. Update the head pointer to the new node.
@@ -42,13 +42,66 @@ class LinkedList {
     temp.next = this.head;
     this.head = temp;
   }
+
+  _printSize() {
+    let curr = this.head;
+    this.size = 0;
+    while (curr !== null) {
+      this.size = this.size + 1;
+      curr = curr.next;
+    }
+
+    console.log(this.size);
+  }
+
+  _getHead() {
+    return this.head;
+  }
+
+  _getTail() {
+    let curr = this.head;
+    while (curr.next !== null) {
+      curr = curr.next;
+    }
+
+    return curr;
+  }
+
+  _atIndex(index) {
+    if (index < 0) return 'index less than 0';
+
+    let curr = this.head;
+    this.size = 0;
+
+    while (this.size !== index) {
+      this.size = this.size + 1;
+      if (curr !== null) {
+        curr = curr.next;
+      } else {
+        console.log('Index larger than node');
+        break;
+      }
+    }
+
+    console.log(curr);
+  }
+
+  // _popNode() {}
 }
 
 const theLink = new LinkedList();
-theLink.append('a');
-theLink.append('b');
-theLink.append('c');
-theLink.append('d');
+theLink._append('a');
+theLink._append('b');
+theLink._append('c');
+theLink._append('d');
 
-theLink.prepend('f');
-console.log(theLink);
+theLink._prepend('firstNode');
+
+// theLink._printSize();
+
+// console.log(theLink._printSize());
+// console.log(theLink._getTail());
+
+// console.log(theLink._atIndex(2));
+// console.log(theLink._atIndex(4));
+theLink._atIndex(4);
